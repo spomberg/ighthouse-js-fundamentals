51d8050e6b3542bd7687d8cea9045c463feac724
Marcos Spomberg <marcosspomberg@gmail.com>
2021-08-26 23:37:38 +0000

    Concatenate arrays challenge

---
+++ concatenate-arrays.js
@@ -0,0 +1,12 @@
+const concat = function (array1, array2){
+  let result = array1;
+  for (let i = 0; i < array2.length; i++){
+    result.push(array2[i]);
+  }
+  return result;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
